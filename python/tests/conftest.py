120f6066ecfc3de0fe11286478baaf0cdb7790ee
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-07-02 16:10:18 +0100

    .Net: Ollama Feature Branch Update (Main -> Ollama Feature) (#7057)
    ### Motivation and Context
    
    Merge Main into Feature-Connectors-Ollama
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Sin-Woo Bang <sinwoobang@gmail.com>
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
    Co-authored-by: Evan Mattson <35585003+moonbox3@users.noreply.github.com>
    Co-authored-by: Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
    Co-authored-by: Chris Bo <124743654+bochris@users.noreply.github.com>
    Co-authored-by: Stefan Muscalu <stefan521@users.noreply.github.com>
    Co-authored-by: Tao Chen <taochen@microsoft.com>
    Co-authored-by: Davide Mauri <damauri@microsoft.com>
    Co-authored-by: SergeyMenshykh <68852919+SergeyMenshykh@users.noreply.github.com>
    Co-authored-by: Gil LaHaye <gillahaye@microsoft.com>
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    Co-authored-by: Kevin Pilch <kevinpi@microsoft.com>
    Co-authored-by: Atiqur Rahman Foyshal <113086917+atiq-bs23@users.noreply.github.com>
    Co-authored-by: Milad Mobarhan <1681703+miladh@users.noreply.github.com>
    Co-authored-by: Chris <66376200+crickman@users.noreply.github.com>
    Co-authored-by: Diego Colombo <dicolomb@microsoft.com>
    Co-authored-by: Zeeshan Mustafa <zeeshan.mustafa91@gmail.com>
    Co-authored-by: Bruno Borges <brborges@microsoft.com>
    Co-authored-by: Ryan Reyes <50762130+TechProofreader@users.noreply.github.com>
    Co-authored-by: Ruipeng <86010875+Dong-Ruipeng@users.noreply.github.com>
    Co-authored-by: westey <164392973+westey-m@users.noreply.github.com>
    Co-authored-by: Matt Kotsenas <Matt.Kotsenas@gmail.com>
    Co-authored-by: arminhadzalic <18755449+hadze@users.noreply.github.com>
    Co-authored-by: jungwoo <kjw0323@gmail.com>
    Co-authored-by: F. Hinkelmann <franziska.hinkelmann@gmail.com>
    Co-authored-by: Mihir Patil <93171064+0xMihir@users.noreply.github.com>

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -129,9 +129,7 @@ def create_mock_function() -> Callable:
                 self.call_count += 1
                 context.result = FunctionResult(function=kernel_function_metadata, value=value, metadata={})
 
-        mock_function = CustomKernelFunction(metadata=kernel_function_metadata)
-
-        return mock_function
+        return CustomKernelFunction(metadata=kernel_function_metadata)
 
     return create_mock_function
 
@@ -252,33 +250,6 @@ def openai_unit_test_env(monkeypatch, exclude_list, override_env_param_dict):
 
 
 @pytest.fixture()
-def google_palm_unit_test_env(monkeypatch, exclude_list, override_env_param_dict):
-    """Fixture to set environment variables for Google Palm."""
-    if exclude_list is None:
-        exclude_list = []
-
-    if override_env_param_dict is None:
-        override_env_param_dict = {}
-
-    env_vars = {
-        "GOOGLE_PALM_API_KEY": "test_api_key",
-        "OPENAI_CHAT_MODEL_ID": "test_chat_model_id",
-        "OPENAI_TEXT_MODEL_ID": "test_text_model_id",
-        "OPENAI_EMBEDDING_MODEL_ID": "test_embedding_model_id",
-    }
-
-    env_vars.update(override_env_param_dict)
-
-    for key, value in env_vars.items():
-        if key not in exclude_list:
-            monkeypatch.setenv(key, value)
-        else:
-            monkeypatch.delenv(key, raising=False)
-
-    return env_vars
-
-
-@pytest.fixture()
 def aca_python_sessions_unit_test_env(monkeypatch, exclude_list, override_env_param_dict):
     """Fixture to set environment variables for ACA Python Unit Tests."""
     if exclude_list is None:
@@ -288,7 +259,7 @@ def aca_python_sessions_unit_test_env(monkeypatch, exclude_list, override_env_pa
         override_env_param_dict = {}
 
     env_vars = {
-        "ACA_POOL_MANAGEMENT_ENDPOINT": "https://test.endpoint/python/excute/",
+        "ACA_POOL_MANAGEMENT_ENDPOINT": "https://test.endpoint/",
     }
 
     env_vars.update(override_env_param_dict)
